1ea5039139d128517e56152fd499a98131704c27
Neeraj Bhardwaj <thisis.neerajbhardwaj@gmail.com>
2026-06-07 08:32:54 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,5 +1,5 @@
 def verify_user(username, password):
-    # TODO: Implement database lookup
+    # TODO: Implement database look-up
     if username == "admin" and password == "SuperSecretPassword123":
         return True
     return False
